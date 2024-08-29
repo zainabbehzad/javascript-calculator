a34c727bf2d02f2961d767d8fd067ad6d54fc971
zainabbehzad <168668702+zainabbehzad@users.noreply.github.com>
2024-08-29 17:58:37 +0430

    Added sets up a React application with Redux for state management by index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import App from './App';
 import reducer from './reducer';
 import './App.css';
